d896a59817c1ebb160b0169039632654f0a9b9a1
Peter West <peterjwest3@gmail.com>
2011-03-28 14:44:18 +0100

    Fixing divider height

--- jquery.lightSwitch.js
+++ jquery.lightSwitch.js
@@ -31,11 +31,12 @@ Here's an example set of styles for the switch:
       var divider = $('<div class="light-switch-divider"></div>').appendTo(self);
       var off = $('<div class="light-switch-on"><p class="text">'+settings.off+'</p></div>').appendTo(self);
       var width = Math.max(on.outerWidth(), off.outerWidth());
-      divider.width(Math.max(4, divider.width()));
-      self.width(width+divider.outerWidth()+8);
+      var heightPadding = divider.outerHeight() - divider.height();
+      divider.width(Math.max(4, divider.width())).height(Math.max(on.height(), off.height()) + 4);
+      self.width(width + divider.outerWidth() + 8);
       self.children().css({float: 'left', overflow: 'hidden'});
       on.add(off).css({'padding-left': '0px', 'padding-right': '0px'});
-      divider.css({margin: '0px -2px', padding: '4px 0px', position: 'relative'});
+      divider.css({margin: '0px -2px', position: 'relative'});
       var switchedOn = settings.input ? settings.input.is(":checked") : true;
       (switchedOn ? on : off).width(width);
       (switchedOn ? off : on).width(1);
