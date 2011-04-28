67a9cb5688fccb2d48e6dee839b5d6232e451f60
Peter West <peterjwest3@gmail.com>
2011-04-28 14:53:55 +0100

    Adding support for varyiable css borders, padding, rounded corners etc.

--- jquery.lightSwitch.js
+++ jquery.lightSwitch.js
@@ -22,29 +22,47 @@ Here's an example set of styles for the switch:
         input: false,
         on: "On",
         off: "Off",
-        onToggle: function() {}
+        onToggle: function() {},
+        borderRadius: 0
       };
       $.extend(settings, options || {});
       var self = $(this).is("input[type=checkbox]") ? $('<div>').insertAfter(settings.input = $(this).hide()) : $(this);
       self.addClass("light-switch").css({float: 'left', cursor: 'pointer'}).children().remove();
       var on = $('<div class="light-switch-on"><p class="text">'+settings.on+'</p></div>').appendTo(self);
       var divider = $('<div class="light-switch-divider"></div>').appendTo(self);
-      var off = $('<div class="light-switch-on"><p class="text">'+settings.off+'</p></div>').appendTo(self);
+      var off = $('<div class="light-switch-off"><p class="text">'+settings.off+'</p></div>').appendTo(self);
+      if (settings.borderRadius > 0) {
+        on.css({'-moz-border-radius': settings.borderRadius+'px', 'border-radius': settings.borderRadius+'px'});
+        divider.css({'-moz-border-radius': settings.borderRadius+'px', 'border-radius': settings.borderRadius+'px'});
+        off.css({'-moz-border-radius': settings.borderRadius+'px', 'border-radius': settings.borderRadius+'px'});
+      }
+      on.css({
+        '-moz-border-radius-bottomright': '0px', '-moz-border-radius-topright': '0px', 
+        'border-bottom-right-radius': '0px', 'border-top-right-radius': '0px'
+      });
+      off.css({
+        '-moz-border-radius-bottomleft': '0px', '-moz-border-radius-topleft': '0px', 
+        'border-bottom-left-radius': '0px', 'border-top-left-radius': '0px'
+      }); 
+      
       var width = Math.max(on.outerWidth(), off.outerWidth());
       var heightPadding = divider.outerHeight() - divider.height();
-      divider.width(Math.max(4, divider.width())).height(Math.max(on.height(), off.height()) + 4);
+      var onMinWidth = (on.outerWidth() - on.width()) + Math.max(parseInt(on.css('border-bottom-left-radius').split(" ")[0]), parseInt(on.css('border-top-left-radius').split(" ")[0]));
+      var offMinWidth = (off.outerWidth() - off.width()) + Math.max(parseInt(off.css('border-bottom-right-radius').split(" ")[0]), parseInt(off.css('border-top-right-radius').split(" ")[0]));
+      alert(onMinWidth + offMinWidth - (divider.outerWidth() - divider.width()));
+      divider.width(Math.max(onMinWidth + offMinWidth - (divider.outerWidth() - divider.width()), divider.width())).height(Math.max(on.height(), off.height()) + 4);
       self.width(width + divider.outerWidth() + 8);
       self.children().css({float: 'left', overflow: 'hidden'});
       on.add(off).css({'padding-left': '0px', 'padding-right': '0px'});
-      divider.css({margin: '0px -2px', position: 'relative'});
+      divider.css({margin: '0px -3px', position: 'relative'});
       var switchedOn = settings.input ? settings.input.is(":checked") : true;
       (switchedOn ? on : off).width(width);
       (switchedOn ? off : on).width(1);
       var toggle = function() {
         switchedOn = !switchedOn;
         if (settings.input) settings.input.attr("checked", switchedOn);
-        (switchedOn ? on : off).animate({ width: width }, 200);
-        (switchedOn ? off : on).animate({ width: 1 }, 200);
+        (switchedOn ? on : off).animate({ width: width }, 150);
+        (switchedOn ? off : on).animate({ width: 1 }, 150);
         settings.onToggle(switchedOn);
         return false;
       };
