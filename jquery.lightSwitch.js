57bea2de23f38b70a8655de3aa8b5d968f0adcf5
Peter West <peterjwest3@gmail.com>
2011-04-29 00:18:44 +0100

    CSS support mostly working now

--- jquery.lightSwitch.js
+++ jquery.lightSwitch.js
@@ -32,37 +32,42 @@ Here's an example set of styles for the switch:
       var divider = $('<div class="light-switch-divider"></div>').appendTo(self);
       var off = $('<div class="light-switch-off"><p class="text">'+settings.off+'</p></div>').appendTo(self);
       if (settings.borderRadius > 0) {
-        on.css({'-moz-border-radius': settings.borderRadius+'px', 'border-radius': settings.borderRadius+'px'});
-        divider.css({'-moz-border-radius': settings.borderRadius+'px', 'border-radius': settings.borderRadius+'px'});
-        off.css({'-moz-border-radius': settings.borderRadius+'px', 'border-radius': settings.borderRadius+'px'});
+        on.css({'-moz-border-radius': settings.borderRadius, 'border-radius': settings.borderRadius});
+        divider.css({'-moz-border-radius': settings.borderRadius, 'border-radius': settings.borderRadius});
+        off.css({'-moz-border-radius': settings.borderRadius, 'border-radius': settings.borderRadius});
       }
       on.css({
-        '-moz-border-radius-bottomright': '0px', '-moz-border-radius-topright': '0px', 
-        'border-bottom-right-radius': '0px', 'border-top-right-radius': '0px'
+        '-moz-border-radius-bottomright': 0, '-moz-border-radius-topright': 0, 
+        'border-bottom-right-radius': 0, 'border-top-right-radius': 0
       });
       off.css({
-        '-moz-border-radius-bottomleft': '0px', '-moz-border-radius-topleft': '0px', 
-        'border-bottom-left-radius': '0px', 'border-top-left-radius': '0px'
+        '-moz-border-radius-bottomleft': 0, '-moz-border-radius-topleft': 0, 
+        'border-bottom-left-radius': 0, 'border-top-left-radius': 0
       }); 
       
       var width = Math.max(on.outerWidth(), off.outerWidth());
       var heightPadding = divider.outerHeight() - divider.height();
-      var onMinWidth = (on.outerWidth() - on.width()) + Math.max(parseInt(on.css('border-bottom-left-radius').split(" ")[0]), parseInt(on.css('border-top-left-radius').split(" ")[0]));
-      var offMinWidth = (off.outerWidth() - off.width()) + Math.max(parseInt(off.css('border-bottom-right-radius').split(" ")[0]), parseInt(off.css('border-top-right-radius').split(" ")[0]));
-      alert(onMinWidth + offMinWidth - (divider.outerWidth() - divider.width()));
-      divider.width(Math.max(onMinWidth + offMinWidth - (divider.outerWidth() - divider.width()), divider.width())).height(Math.max(on.height(), off.height()) + 4);
-      self.width(width + divider.outerWidth() + 8);
+      on.css({'padding-left': 0, 'padding-right': 0});
+      off.css({'padding-left': 0, 'padding-right': 0});
+      var onSpacing = on.outerWidth() - on.width();
+      var offSpacing = off.outerWidth() - off.width();
+      var dividerSpacing = divider.outerWidth() - divider.width();
+      var onMinWidth = onSpacing + Math.max(parseInt(on.css('border-bottom-left-radius').split(" ")[0]), parseInt(on.css('border-top-left-radius').split(" ")[0]));      
+      var offMinWidth = offSpacing + Math.max(parseInt(off.css('border-bottom-right-radius').split(" ")[0]), parseInt(off.css('border-top-right-radius').split(" ")[0]));
+      on.children().css({'padding-right': onMinWidth + onSpacing - 1 });
+      off.children().css({'padding-left': offMinWidth + offSpacing - 1 });
+      divider.width(Math.max(onMinWidth + onSpacing + offMinWidth + offSpacing - dividerSpacing, divider.width())).height(Math.max(on.height(), off.height()) + 4);
+      self.width(width + divider.outerWidth() + onMinWidth + offMinWidth);
       self.children().css({float: 'left', overflow: 'hidden'});
-      on.add(off).css({'padding-left': '0px', 'padding-right': '0px'});
-      divider.css({margin: '0px -3px', position: 'relative'});
+      divider.css({'margin-right': -(offMinWidth + offSpacing), 'margin-left': -(onMinWidth + onSpacing), position: 'relative'});
       var switchedOn = settings.input ? settings.input.is(":checked") : true;
       (switchedOn ? on : off).width(width);
-      (switchedOn ? off : on).width(1);
+      (switchedOn ? off : on).width(switchedOn ? offMinWidth : onMinWidth);
       var toggle = function() {
         switchedOn = !switchedOn;
         if (settings.input) settings.input.attr("checked", switchedOn);
         (switchedOn ? on : off).animate({ width: width }, 150);
-        (switchedOn ? off : on).animate({ width: 1 }, 150);
+        (switchedOn ? off : on).animate({ width: switchedOn ? offMinWidth : onMinWidth }, 150);
         settings.onToggle(switchedOn);
         return false;
       };
